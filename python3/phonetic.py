cf8d505a944386b34784f71c38d8bb2938133d63
Brandon Amos <bdamos@vt.edu>
2014-04-16 08:55:46 -0400

    Formatting.

--- python3/phonetic.py
+++ python3/phonetic.py
@@ -9,32 +9,12 @@
 import sys
 
 phonetic_table = {
-  'a': 'alpha',
-  'b': 'bravo',
-  'c': 'charlie',
-  'd': 'delta',
-  'e': 'echo',
-  'f': 'foxtrot',
-  'g': 'golf',
-  'h': 'hotel',
-  'i': 'india',
-  'j': 'juliet',
-  'k': 'kilo',
-  'l': 'lima',
-  'm': 'mike',
-  'n': 'november',
-  'o': 'oscar',
-  'p': 'papa',
-  'q': 'quebec',
-  'r': 'romeo',
-  's': 'sierra',
-  't': 'tango',
-  'u': 'uniform',
-  'v': 'victor',
-  'w': 'whiskey',
-  'x': 'x-ray',
-  'y': 'yankee',
-  'z': 'zulu',
+  'a': 'alpha', 'b': 'bravo', 'c': 'charlie', 'd': 'delta', 'e': 'echo',
+  'f': 'foxtrot', 'g': 'golf', 'h': 'hotel', 'i': 'india', 'j': 'juliet',
+  'k': 'kilo', 'l': 'lima', 'm': 'mike', 'n': 'november', 'o': 'oscar',
+  'p': 'papa', 'q': 'quebec', 'r': 'romeo', 's': 'sierra', 't': 'tango',
+  'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x': 'x-ray',
+  'y': 'yankee', 'z': 'zulu',
 }
 
 for word in sys.argv[1:]:
