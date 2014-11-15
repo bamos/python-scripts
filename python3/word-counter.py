2251d949ba32f52f1ff1ca8734d746f74e77ef6d
Brandon Amos <bamos@cmu.edu>
2014-11-15 18:05:23 -0500

    Use itertoolz.sliding_window.

--- python3/word-counter.py
+++ python3/word-counter.py
@@ -3,13 +3,7 @@
 from collections import Counter
 import argparse
 import re
-from itertools import islice
-import operator
-
-url_re = re.compile(
-  'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-)
-
+from toolz.itertoolz import sliding_window
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--numWords',type=int,default=10)
@@ -18,29 +12,22 @@ parser.add_argument('--minWordLength',type=int,default=5)
 parser.add_argument('file',type=str)
 args = parser.parse_args()
 
-# Inspired by http://stackoverflow.com/questions/6822725
-def window(seq, n):
-  it = iter(seq)
-  result = tuple(islice(it, n))
-  if len(result) == n:
-    yield result
-  for elem in it:
-    result = result[1:] + (elem,)
-    useThisWindow = True
-    for i in result:
-      if len(i) < args.minWordLength:
-        useThisWindow = False
-        break
-      elif url_re.search(i):
-        useThisWindow = False
-        break
-    if useThisWindow:
-      yield result
+def filter_func(tup):
+  for word in tup:
+    if len(word) < args.minWordLength:
+      return False
+    elif re.search('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',word):
+      return False
+  else:
+    return True
 
+def filtered_window(seq,n):
+  return filter(filter_func, sliding_window(n,seq))
+      
 with open(args.file,'r') as f:
   content = f.read().replace('\n',' ').lower()
   words = re.findall(r'\S+', content)
   for i in range(1,args.maxTuples+1):
     print("\n=== Sliding Window: {} ===".format(i))
-    for tup in Counter(window(words,i)).most_common(args.numWords):
+    for tup in Counter(filtered_window(words,i)).most_common(args.numWords):
       print("  {}: '{}'".format(tup[1]," ".join(tup[0])))
