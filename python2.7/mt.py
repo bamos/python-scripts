632c95816ba77fcfd636d598346528b780efb4c5
Brandon Amos <bdamos@vt.edu>
2015-03-24 08:34:39 -0400

    mt.py: Disable output buffering.

--- python2.7/mt.py
+++ python2.7/mt.py
@@ -2,6 +2,14 @@
 
 import argparse
 import multitail
+import sys
+
+# http://stackoverflow.com/questions/107705
+class Unbuffered(object):
+  def __init__(self, stream): self.stream = stream
+  def write(self, data): self.stream.write(data); self.stream.flush()
+  def __getattr__(self, attr): return getattr(self.stream, attr)
+sys.stdout = Unbuffered(sys.stdout)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('files', type=str, nargs='+')
