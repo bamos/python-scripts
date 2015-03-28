81d65e867eb507f56e01c7fae05832d54f53f892
Brandon Amos <bdamos@vt.edu>
2015-03-28 13:05:38 -0400

    Run pep on all scripts for #4.

--- .travis-script-2.sh
+++ .travis-script-2.sh
@@ -3,3 +3,7 @@ set -x -e # Show commands being executed and exit nonzero upon errors.
 
 ./python2.7/music-organizer.py --help
 ./python2.7/mt.py --help
+
+for F in python2.7/*; do
+  pep8 --ignore=E402 $F
+done
--- .travis-script-3.sh
+++ .travis-script-3.sh
@@ -10,3 +10,7 @@ set -x -e # Show commands being executed and exit nonzero upon errors.
 ./python3/word-counter.py README.md
 ./python3/eval-expr.py '(((4+6)*10)<<2)'
 ./python3/merge-mutt-contacts.py --help
+
+for F in python3/*; do
+  pep8 --ignore=E402 $F
+done
